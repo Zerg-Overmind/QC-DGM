d248bb4a3757ed82c1f5f856f9790f8ec48a2d25
quankaiGao <563041763@qq.com>
2021-04-15 10:01:54 +0800

    general setting

--- utils/fm_loss.py
+++ utils/fm_loss.py
@@ -22,9 +22,9 @@ class FMLoss(nn.Module):
         loss_n = torch.tensor(0.).to(pred_perm.device)
         n_sum = torch.tensor(0.).to(pred_perm.device)
 
- 
+        ## general setting
         alpha = 2
-        beta = 0.1
+        beta = 2
         for b in range(batch_num):
             ## false positive
             loss_p += torch.sum(torch.mul(pred_perm[b, :pred_ns[b], :gt_ns[b]], (1 - gt_perm[b, :pred_ns[b], :gt_ns[b]])))
@@ -32,8 +32,9 @@ class FMLoss(nn.Module):
             loss_n += torch.sum(torch.mul(gt_perm[b, :pred_ns[b], :gt_ns[b]], (1 - pred_perm[b, :pred_ns[b], :gt_ns[b]])))
 
             n_sum += pred_ns[b].to(n_sum.dtype).to(pred_perm.device)
-        return torch.exp(alpha * loss_n / n_sum) - 1 + torch.exp(beta * loss_p / n_sum) - 1
-           # a more general setting
+        return (torch.exp(alpha * loss_n / n_sum) + torch.exp(beta * loss_p / n_sum))/4 
+ 
+           # different weights according to data
            # alpha = 2
-           # beta = 2
-           # return (torch.exp(alpha * loss_n / n_sum) + torch.exp(beta * loss_p / n_sum))/4  
+           # beta = 0.1
+           # return torch.exp(alpha * loss_n / n_sum) + torch.exp(beta * loss_p / n_sum)
