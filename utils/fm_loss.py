e41dacacc58c6aa3c32a130d724c33856fa0332d
quankaiGao <563041763@qq.com>
2021-05-16 09:50:34 +0800

    add

--- utils/fm_loss.py
+++ utils/fm_loss.py
@@ -22,9 +22,9 @@ class FMLoss(nn.Module):
         loss_n = torch.tensor(0.).to(pred_perm.device)
         n_sum = torch.tensor(0.).to(pred_perm.device)
 
-        ## general setting
+       
         alpha = 2
-        beta = 2
+        beta = 0 # because of Sinkhorn normalization, beta can be very small (also can be 0.1)
         for b in range(batch_num):
             ## false positive
             loss_p += torch.sum(torch.mul(pred_perm[b, :pred_ns[b], :gt_ns[b]], (1 - gt_perm[b, :pred_ns[b], :gt_ns[b]])))
@@ -32,9 +32,6 @@ class FMLoss(nn.Module):
             loss_n += torch.sum(torch.mul(gt_perm[b, :pred_ns[b], :gt_ns[b]], (1 - pred_perm[b, :pred_ns[b], :gt_ns[b]])))
 
             n_sum += pred_ns[b].to(n_sum.dtype).to(pred_perm.device)
-        return (torch.exp(alpha * loss_n / n_sum) + torch.exp(beta * loss_p / n_sum))/4 
+        return torch.exp(alpha * loss_n / n_sum) + torch.exp(beta * loss_p / n_sum)
  
-           ## different weights according to data, you can also set beta=0 because of sinkhorn normalization
-           # alpha = 2
-           # beta = 0.1
-           # return torch.exp(alpha * loss_n / n_sum) + torch.exp(beta * loss_p / n_sum)
+          
